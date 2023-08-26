d456586478eee04a8ccb72505962618e8b8fc1be
dominic-amalu <dominique.amalu@gmail.com>
2023-08-26 09:46:58 +0100

    error message

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -6,32 +6,38 @@ import prisma from '@/app/libs/prismadb';
 export async function POST(request: Request) {
   const body = await request.json();
   const { email, name, password } = body;
-
-  const checkIfUserExists = await prisma.user.findUnique({
-    where: {
-      email: email,
-    },
-  });
-  if (checkIfUserExists) {
-    return NextResponse.json(
-      { message: 'User already Registered' },
-      { status: 401 }
-    );
-  } else {
-    const viewedProfile = Math.floor(Math.random() * 1000);
-    const impressions = Math.floor(Math.random() * 1000);
-    const hashedPassword = await bcrypt.hash(password, 12);
-
-    const user = await prisma.user.create({
-      data: {
-        email,
-        name,
-        hashedPassword,
-        viewedProfile,
-        impressions,
+  try {
+    const checkIfUserExists = await prisma.user.findUnique({
+      where: {
+        email: email,
       },
     });
+    if (checkIfUserExists) {
+      return NextResponse.json(
+        { message: 'User already Registered' },
+        { status: 401 }
+      );
+    } else {
+      const viewedProfile = Math.floor(Math.random() * 1000);
+      const impressions = Math.floor(Math.random() * 1000);
+      const hashedPassword = await bcrypt.hash(password, 12);
 
-    return NextResponse.json(user);
+      const user = await prisma.user.create({
+        data: {
+          email,
+          name,
+          hashedPassword,
+          viewedProfile,
+          impressions,
+        },
+      });
+
+      return NextResponse.json(user);
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'An error occured while registering the user' },
+      { status: 500 }
+    );
   }
 }
