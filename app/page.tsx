9b7d511c9dc6406736a20371fb34e56d4b4086d9
dominic-amalu <dominique.amalu@gmail.com>
2023-08-28 17:18:01 +0100

    hello

--- app/page.tsx
+++ app/page.tsx
@@ -23,11 +23,12 @@ export default async function Home() {
   return (
     <ClientOnly>
       <Container>
-        <HomeCard
+        <div>hello</div>
+        {/* <HomeCard
           currentUser={currentUser}
           // feed={feed}
           // friendList={friendList}
-        />
+        /> */}
       </Container>
     </ClientOnly>
   );
