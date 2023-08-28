9ff18f2753dd42684e572145bd06650b551d44f7
dominic-amalu <dominique.amalu@gmail.com>
2023-08-28 17:29:08 +0100

    feed

--- app/page.tsx
+++ app/page.tsx
@@ -7,22 +7,23 @@ import EmptyState from './components/EmptyState';
 import HomeCard from './components/home/HomeCard';
 
 export default async function Home() {
-  const currentUser = await getCurrentUser();
+  // const currentUser = await getCurrentUser();
 
   // const feed = await getFeedPosts();
   // const friendList = await getUsersFriends({ userId: currentUser?.id });
 
-  if (!currentUser) {
-    return (
-      <ClientOnly>
-        <EmptyState showReset />
-      </ClientOnly>
-    );
-  }
+  // if (!currentUser) {
+  //   return (
+  //     <ClientOnly>
+  //       <EmptyState showReset />
+  //     </ClientOnly>
+  //   );
+  // }
 
   return (
     <ClientOnly>
       <Container>
+        <div>feed</div>
         {/* <HomeCard
           currentUser={currentUser}
           // feed={feed}
